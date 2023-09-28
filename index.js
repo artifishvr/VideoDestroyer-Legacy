9ca0d8b61a4264ba0e6da919eb31f7eb34cde6e6
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-09-27 23:12:11 -0700

    enhance the spaghetti

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ program
     .requiredOption('-o, --output <file>', 'output file')
     .option('-b, --bitrate <rate>', 'video bitrate')
     .option('--bassboost <value>', 'video bass boost')
-    .option('-v, --volume <value>', 'video volume boost');
-// TODO add more options
+    .option('-v, --volume <value>', 'video volume boost')
+    .option('-f, --fps <value>', 'video framerate')
+    .option('-r, --resolution <value>', 'video resolution')
 
 program.parse(process.argv);
 
@@ -18,17 +19,28 @@ console.log('Output file: ', options.output);
 
 
 const command = ffmpeg(options.input)
-    .videoBitrate(options.bitrate || '1')
+    .outputOptions([
+        '-preset veryfast',
+        `-minrate ${options.bitrate || 1}`,
+        `-maxrate ${options.bitrate || 1}`,
+        `-bufsize ${options.bitrate || 1}`,
+    ])
+    .videoBitrate(options.bitrate || 1)
     .audioFilter(`bass=g=${options.bassboost || '5'}`)
     .audioFilter(`volume=${options.volume || '5'}dB`)
-    .audioCodec('libmp3lame')
-    .audioBitrate('2')
-    .fps(5)
-    .size('320x240')
+    .audioBitrate('1')
+    .fps(options.fps || 5)
+    .size(options.resolution || '100x100')
     .output(options.output);
 
+const command2 = ffmpeg(options.output)
+    .size('1920x1080')
+    .videoBitrate(1)
+    .output(options.output + "2.mp4");
+
 command.on('end', () => {
-    console.log('Finished processing');
+    console.log('Finished processing, doing it again');
+    command2.run();
 });
 
 command.on('error', (err) => {
@@ -36,3 +48,10 @@ command.on('error', (err) => {
 });
 
 command.run();
+
+command2.on('end', () => {
+    console.log('Finished processing');
+});
+command2.on('error', (err) => {
+    console.error(err);
+});
